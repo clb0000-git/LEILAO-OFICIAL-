7e5fa88882a332dfa7c3e7ccef435a170fb4078c
clb0000-git <caualuisborges19@gmail.com>
2026-04-20 15:21:42 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Teste geral
+document.addEventListener("DOMContentLoaded", () => {
+
+  // BOTÃO ENTRAR
+  const loginBtn = document.getElementById("loginBtn");
+  const modal = document.getElementById("authModal");
+
+  if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+      modal.style.display = "block";
+    });
+  }
+
+  // FECHAR MODAL
+  document.querySelectorAll(".close").forEach(btn => {
+    btn.addEventListener("click", () => {
+      document.querySelectorAll(".modal").forEach(m => {
+        m.style.display = "none";
+      });
+    });
+  });
+
+});
